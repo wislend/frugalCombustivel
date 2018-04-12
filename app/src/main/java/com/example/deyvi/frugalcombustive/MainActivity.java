23bb6f6af5e4b36fdc0cd7c1805ae25fad6a7147
wislend <wislen.souza@aedu.com>
2018-04-12 01:10:32 -0300

    criando entidade e tela de apresentacao do app

--- app/src/main/java/com/example/deyvi/frugalcombustive/MainActivity.java
+++ app/src/main/java/com/example/deyvi/frugalcombustive/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.deyvi.frugalcombustive;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
 
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    @AfterViews
+    public void init() {
+
     }
+
 }
+
+
+
+
